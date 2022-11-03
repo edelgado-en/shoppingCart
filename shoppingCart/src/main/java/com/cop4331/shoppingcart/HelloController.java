c0649b0af7fe05cca9229e6c56da9d0fe94bb071
edelgado-git <enriquedelgado806@gmail.com>
2022-11-03 14:29:34 -0400

    Initial commit

---
+++ shoppingCart/src/main/java/com/cop4331/shoppingcart/HelloController.java
@@ -0,0 +1,14 @@
+package com.cop4331.shoppingcart;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
